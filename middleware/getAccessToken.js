8e5a63f5859b79b71fb24d533ba3bc00c76dc4b4
Guille-Rubio <guillermorubiog@gmail.com>
2023-07-11 12:45:29 +0200

    UPD/get access token from auth header

--- middleware/getAccessToken.js
+++ middleware/getAccessToken.js
@@ -3,8 +3,20 @@ const getAccessToken = express.Router();
 
 
 getAccessToken.use(async (req, res, next) => {
-    const { cookie } = req.headers;
-    if (cookie.includes(`access_token=`)) {
+    const { cookie, authorization } = req.headers;
+
+    //Check authorization header
+    if (authorization.includes(`Bearer`)) {
+        const token = authorization.split(' ')[1];
+        if (token) {
+            req.token = token;
+            next();
+        } else {
+            res.sendStatus(403)
+        }
+
+    //Check cookies
+    } else if (cookie.includes(`access_token=`)) {
         const cookies = cookie.split(',');
         const accessToken = cookies[0];
         const token = accessToken.split('=')[1];
